3cb1c9371fc8804cf8bb724597d1d283bd7ebe82
Dongyu Wu <dongyu@Dongyus-MacBook-Pro.local>
2019-09-10 15:17:36 -0600

    fix

--- space.sh
+++ space.sh
@@ -4,11 +4,11 @@ ROOT_USE=$(df / | awk '/[0-9]{1,2}%/{print $5-0}')
 BOOT_USE=$(df /boot | awk '/[0-9]{1,2}%/{print $5-0}')
 if [ $ROOT_USE -ge $THRES ]
 then
-        SUBBJECT="/ utilization exceeds $THRES %"
+        SUBJECT="/ utilization exceeds $THRES %"
         mail -s "$SUBJECT" root@localhost < /dev/null
 fi
 if [ $BOOT_USE -ge $THRES ]
 then
-        SUBBJECT="/boot utilization exceeds $THRES %"
+        SUBJECT="/boot utilization exceeds $THRES %"
         mail -s "$SUBJECT" root@localhost < /dev/null
 fi
